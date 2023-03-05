80af118386df6393c0d5bf2e4b2784ab71f398a8
Granderkos <77847498+Granderkos@users.noreply.github.com>
2023-03-05 17:44:49 +0100

    added default value for 'games'

--- profiles/ss-profile.php
+++ profiles/ss-profile.php
@@ -1,89 +1,8 @@
 <?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/login-register.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <button id="back" onclick="back()">
-        <i class="fas fa-arrow-left"></i>
-    </button>
-</head>
-
-<body>
-    <!-- register from -->
-    <div class="form-container">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label for="username" class="label-control">Username:</label>
-                <input type="text" class="form-control input-field" id="username" name="username" placeholder="Username"required>
-            </div>
-            <div class="form-group">
-                <label for="password" class="label-control">Password:</label>
-                <input type="password" class="form-control input-field" id="password" name="password" placeholder="Password" required>
-            </div>
-            <div class="form-group">
-                <label for="passworda" class="label-control">Password again:</label>
-                <input type="password" class="form-control input-field" id="passworda" name="passworda" placeholder="Password"required>
-            </div>
-            <div class="form-group form-check">
-                <label class="form-check-label" for="login">Log me in
-                    <input type="checkbox" class="form-check-input" id="login" name="login">
-                    <span class="checkmark"></span>
-                </label>
-            </div>
-            <button type="submit" class="btn btn-primary" name="login_btn">Submit</button>
-        </form>
-    </div>
-    <!-- makes the user into database -->
-    <?php
-    if (isset($_POST['login_btn'])) {
-        include('connectdb.php');
-        $username = ($_POST['username']);
-        $password = $_POST['password'];
-        $passworda = $_POST['passworda'];
-        $pass = password_hash($password, PASSWORD_DEFAULT);
-        $q = $conn->query("SELECT * FROM userdata WHERE username = '$username'");
-        $num = mysqli_num_rows($q);
-        if ($num >= 1) {
-            //user already exists so guest goes back to guest gui
-            echo "<p class='text-danger'>Username already exists</p>";
-        } else if ($password != $passworda) {
-            //password is different that password again or vice versa
-            echo "<p class='text-danger'>Passwords don't match</p>";
-        } else if (strlen($username) > 20) {
-            //username is longer that 20 characters
-            echo "<p class='text-danger'>Username is too long</p>";
-        } else {
-            $date = date("Y-m-d");
-            // sends sends logged user into user gui
-            $conn->query("INSERT INTO userdata (username, pass, creation) values('$username','$pass','$date')");
-            $conn->query("INSERT INTO aimtrainer_data (username, highscore, games) values('$username',0, 0)");
-            $conn->query("INSERT INTO game2_data (username, highscore, games) values('$username',0, 0)");
-            if (!empty($_POST['login'])) {
-                $_SESSION['username'] = strtolower($username);
-                header('location: ../user/index.php');
-                echo '<script> sessionStorage.setItem("username", ' . strtolower($username) . ') </script>';
-            } else {
-                header('location: ../guest/index.php');
-            }
-            if (!file_exists('../profiles/')) {
-                mkdir('../profiles/', 0777, true);
-            }
-            //HTML and PHP creation for new users profile
-            $File = fopen("../profiles/" . $username . "-profile.php", "w");
-            fwrite($File, '<?php
             session_start();
             include("../controller/connectdb.php");
             $username = explode("-", basename(__FILE__, ".php"))[0];
-            $creation = mysqli_fetch_row($conn->query("SELECT * FROM userdata WHERE username = '."'$username'".'"))[3];
+            $creation = mysqli_fetch_row($conn->query("SELECT * FROM userdata WHERE username = 'ss'"))[3];
             $format = explode("-", $creation);
             $bool = false;
             $select = false;
@@ -127,29 +46,29 @@ session_start();
                                 </form>
                                 <?php
                                 if (file_exists("../images/banner/$username-banner.png")) {
-                                    echo '."'<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/$username-banner.png' . ')`;</script>'".';
+                                    echo '<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/ss-banner.png' . ')`;</script>';
                                 }
                                 else if (file_exists("../images/banner/default_banner.png")) {
-                                    echo '."'<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/default_banner.png' . ')`;</script>'".';
+                                    echo '<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/default_banner.png' . ')`;</script>';
                                 }
                             }
                             else
                             {
                                 if (file_exists("../images/banner/$username-banner.png")) {
-                                    echo '."'<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/pp-banner.png' . ')`;</script>'".';
+                                    echo '<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/pp-banner.png' . ')`;</script>';
                                 }
                                 else if (file_exists("../images/banner/default_banner.png")) {
-                                    echo '."'<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/default_banner.png' . ')`;</script>'".';
+                                    echo '<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/default_banner.png' . ')`;</script>';
                                 }
                             }
                         }
                         else
                         {
                             if (file_exists("../images/banner/$username-banner.png")) {
-                                echo '."'<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/$username-banner.png' . ')`;</script>'".';
+                                echo '<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/ss-banner.png' . ')`;</script>';
                             }
                             else if (file_exists("../images/banner/default_banner.png")) {
-                                echo '."'<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/default_banner.png' . ')`;</script>'".';
+                                echo '<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/default_banner.png' . ')`;</script>';
                             }
                         } ?>
                     </div>
@@ -165,7 +84,7 @@ session_start();
                                     } else {
                                         $_FILES["changebanner"]["name"] = $username."-banner.png";
                                         move_uploaded_file($_FILES["changebanner"]["tmp_name"], "../images/banner/" . $_FILES["changebanner"]["name"]);
-                                        echo '."'<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/$username-banner.png' . ')`;</script>'".';
+                                        echo '<script>document.querySelector(`.banner`).style.backgroundImage = `url(' . '../images/banner/ss-banner.png' . ')`;</script>';
                                     }
                                 }
                             }
@@ -197,10 +116,10 @@ session_start();
                                 <?php
                             }
                             if (file_exists("../images/pfp/$username-pfp.png")) {
-                                echo '."'<script>document.querySelector(`.pfp`).style.backgroundImage = `url(' . '../images/pfp/$username-pfp.png' . ')`;</script>'".';
+                                echo '<script>document.querySelector(`.pfp`).style.backgroundImage = `url(' . '../images/pfp/ss-pfp.png' . ')`;</script>';
                             }
                             else if (file_exists("../images/pfp/default_pfp.png")) {
-                                echo '."'<script>document.querySelector(`.pfp`).style.backgroundImage = `url(' . '../images/pfp/default_pfp.png' . ')`;</script>'".';
+                                echo '<script>document.querySelector(`.pfp`).style.backgroundImage = `url(' . '../images/pfp/default_pfp.png' . ')`;</script>';
                             }
                             //pfp change
                             if (isset($_FILES["changepfp"])) {
@@ -211,7 +130,7 @@ session_start();
                                     } else {
                                         $_FILES["changepfp"]["name"] = $username."-pfp.png";
                                         move_uploaded_file($_FILES["changepfp"]["tmp_name"], "../images/pfp/" . $_FILES["changepfp"]["name"]);
-                                        echo '."'<script>document.querySelector(`.pfp`).style.backgroundImage = `url(' . '../images/pfp/$username-pfp.png' . ')`;</script>'".';
+                                        echo '<script>document.querySelector(`.pfp`).style.backgroundImage = `url(' . '../images/pfp/ss-pfp.png' . ')`;</script>';
                                     }
                                 }
                             }
@@ -251,7 +170,7 @@ session_start();
                                         if(strlen($_COOKIE["selected"]) > 0)
                                         {
                                             $name = $_COOKIE["selected"];
-                                            echo mysqli_fetch_row($conn->query("SELECT games FROM ".$name."_data WHERE username = '."'$username'".'"))[0]; 
+                                            echo mysqli_fetch_row($conn->query("SELECT games FROM ".$name."_data WHERE username = 'ss'"))[0]; 
                                         }
                                     }
                                     ?>
@@ -269,7 +188,7 @@ session_start();
                                     {
                                     if (strlen($_COOKIE["selected"]) > 0) {
                                         $name = $_COOKIE["selected"];
-                                        echo mysqli_fetch_row($conn->query("SELECT highscore FROM " . $name . "_data WHERE username = '."'$username'".'"))[0];
+                                        echo mysqli_fetch_row($conn->query("SELECT highscore FROM " . $name . "_data WHERE username = 'ss'"))[0];
                                     }
                                     }
                                     ?>
@@ -290,14 +209,4 @@ session_start();
                 <script src="../js/select.js"></script>
             </body>
             
-            </html>');
-            fclose($File);
-        }
-        $conn->close();
-    }
-    ?>
-    <!-- script for arrow in left upper corner -->
-    <script src="../js/back.js"></script>
-</body>
-
-</html>
+            </html>
